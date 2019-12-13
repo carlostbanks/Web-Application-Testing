2534b3865cc0672be6b6c6f7fdc9003f64a60ea0
CLYPR Technologies <cbanks@clypr.co>
2019-12-12 22:04:48 -0500

    completed baseball game

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [fouls, setFouls] = useState(0);
+
+  const manageStrikes = () => {
+    if (strikes < 3) {
+      setStrikes(strikes + 1);
+    } else {
+      setStrikes(0);
+      setBalls(0);
+      setFouls(0);
+    }
+  }
+
+  const manageBalls = () => {
+    if (balls < 4) {
+      setBalls(balls + 1);
+    } else {
+      setBalls(0);
+    }
+  }
+
+  const manageFouls = () => {
+    if (strikes < 2) {
+      setFouls(fouls + 1);
+      setStrikes(strikes + 1);
+    } else {
+      setFouls(fouls + 1);
+    }
+  }
+
+  const manageHits = () => {
+    setStrikes(0);
+    setBalls(0);
+    setFouls(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Baseball Game</h1>
+      <div><strong>DISPLAY</strong></div>
+      <div>Strikes: {strikes}</div>
+      <div>Balls: {balls}</div>
+      <div>Foulballs: {fouls}</div>
+      <br></br>
+      <br></br>
+      <div><strong>DASHBOARD</strong></div>
+      <button onClick={manageStrikes}>Strikes</button>
+      <button onClick={manageBalls}>Balls</button>
+      <button onClick={manageFouls}>Fouls</button>
+      <button onClick={manageHits}>Hit</button>
     </div>
   );
 }
