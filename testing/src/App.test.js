1a9bd1cfbe3220e0f3562cc23569e6893fd7f983
CLYPR Technologies <cbanks@clypr.co>
2019-12-12 22:21:40 -0500

    completed MVP

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
    import * as rtl from '@testing-library/react';
    import '@testing-library/jest-dom/extend-expect';
+   import App from './App';
 
    afterEach(rtl.cleanup);
 
    it('renders without crashing', () => {
-     // ready to test!
+     const wrapper = rtl.render(<App />)
    });
+
+   test('it renders', () => {
+    const wrapper = rtl.render(<App />)
+    const element = wrapper.getByText(/hit/i)
+    expect(element).toBeInTheDocument()
+   })
+
+   test('numbers render', () => {
+     const wrapper = rtl.render(<App/>)
+     wrapper.getByDisplayValue('0');
+   })
